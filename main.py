59397539c90ff51f0bc2a87a64037e9e9d4370ae
EthanSMorse <emorse@brooklynsteamcenter.org>
2024-02-01 10:54:26 -0500

    les go

--- main.py
+++ main.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def index ():
     return render_template("home.html.jinja")
+
+@app.route('/registration')
+def registration ():
+    return render_template("registration.html.jinja")
