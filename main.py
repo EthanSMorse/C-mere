c122d615f7feecbc204c8f28b0475fa1921783c5
EthanSMorse <emorse@brooklynsteamcenter.org>
2024-02-09 09:01:38 -0500

    sign in done

--- main.py
+++ main.py
@@ -3,8 +3,41 @@ import pymysql
 import pymysql.cursors
 from flask_httpauth import HTTPBasicAuth
 from werkzeug.security import generate_password_hash, check_password_hash
+import flask_login
 
 app = Flask(__name__)
+app.secret_key = "UR7XLL009"
+
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+class User:
+    is_authenticated = True
+    is_anonymous = False
+    is_active = True
+
+    def __init__(self, id, username, email):
+        self.id = id
+        self.username = username
+        self.email = email
+
+    def get_id(self):
+        return str(self.id)
+    
+@login_manager.user_loader
+def load_user(user_id):
+    cursor = conn.cursor()
+    cursor.execute(f"SELECT * FROM `User` WHERE `id` = {user_id}")
+    result = cursor.fetchone()
+    cursor.close()
+    conn.commit()
+
+    if result is None:
+        return None
+    
+    return User(result["ID"], result["Username"], result["Email"])
+
+
 
 conn = pymysql.connect(
     database="emorse_",
@@ -38,11 +71,20 @@ def signup ():
         password = request.form["password"]
         cursor = conn.cursor()
         cursor.execute(f"SELECT * FROM `User` WHERE `Username` = '{username}'")
-        user = cursor.fetchone()
+        result = cursor.fetchone()
         cursor.close()
         conn.commit()
-        if user["Password"] == password:
+        if result["Password"] == password:
+            user = load_user(result['ID'])
+            flask_login.login_user(user)
+
             return redirect('/feed')
 
-
     return render_template("signin.html.jinja")
+
+@app.route('/feed')
+
+@flask_login.login_required
+
+def post_feed():
+    return 'feed page'
